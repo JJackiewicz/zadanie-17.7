c8b1df22c13f944b9111cb5e0c581c93ad6b78d0
Jacek Jackiewicz <jjackiewicz86@gmail.com>
2018-07-17 22:55:34 +0200

    Task 17.7

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '217553890977-m33ls5kbeca6ku64ns839g8fcoqibvp6.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'FCV4xVwZUZbmz8NqGb_x5_rC',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+  
